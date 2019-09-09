f6091c9dde66867ad1938548c6a470a07f820262
hemin <hemin@kdniao.cn>
2019-09-09 20:26:06 +0800

    quick start

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import "github.com/gin-gonic/gin"
+import "net/http"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", index)
+	router.GET("/ping", ping)
+	router.Run()
+}
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
 	})
-	r.Run()
+}
+
+func index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{ "title": "Main website",	})
 }
